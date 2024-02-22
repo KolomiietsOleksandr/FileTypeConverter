119f1534e0383ab21a85f9c12605e4858430a7ad
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-22 14:30:20 +0200

    Homepage design

---
+++ express/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mainHandler = require('./handlers/mainHandler');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.redirect('/main');
+});
+
+app.get('/main', mainHandler.main);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ express/handlers/mainHandler.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+function main(req, res) {
+    res.render('main');
+}
+
+module.exports = {
+  main
+};
