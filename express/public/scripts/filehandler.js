2a3fb7a31b7131a433e6133c5dd98a3b773bce0e
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-27 02:59:29 +0200

    Fixes

--- express/public/scripts/filehandler.js
+++ express/public/scripts/filehandler.js
@@ -1,82 +1,98 @@
 function handleFileSelect(event) {
     const file = event.target.files[0];
 
-    document.getElementById('fileName').innerText = `${file.name}`;
-    document.getElementById('fileSize').innerText = `${(file.size * 0.0000010).toFixed(2)}MB`;
-    document.getElementById('fileFormat').innerText = `${file.type.split('/')[0]}`;
+    var supportedFormats = ['JPEG', 'PNG', 'GIF', 'BMP', 'WAV', 'MP3'];
 
-    document.getElementById('zone').style.display = 'none';
-    document.getElementById('converterPanel').style.display = 'block';
-
-    let conversionFormats;
-    switch (file.type.split('/')[0]) {
-        case 'image':
-            conversionFormats = ['JPEG', 'PNG', 'GIF', 'BMP'];
-            break;
-        case 'application':
-            conversionFormats = ['PDF', 'DOCX', 'TXT', 'RTF'];
-            break;
-        case 'video':
-            conversionFormats = ['MP4', 'GIF', 'AVI', 'MOV'];
-            break;
-        case 'audio':
-            conversionFormats = ['WAV', 'MP3'];
-            break;
-        default:
-            conversionFormats = [];
-            break;
+    if (!supportedFormats.includes(file.type.split('/')[1].toUpperCase())) {
+        document.getElementById('unsup').style.display = 'block';
     }
+    else {
+        document.getElementById('fileName').innerText = `${file.name}`;
+        document.getElementById('fileSize').innerText = `${(file.size * 0.0000010).toFixed(2)}MB`;
+        document.getElementById('fileFormat').innerText = `${file.type.split('/')[0]}`;
 
-    const selectElement = document.getElementById('formatSelect');
-    selectElement.innerHTML = '';
+        document.getElementById('unsup').style.display = 'none';
+        document.getElementById('zone').style.display = 'none';
+        document.getElementById('converterPanel').style.display = 'block';
 
-    conversionFormats.forEach(format => {
-        const option = document.createElement('option');
-        option.value = format;
-        option.textContent = format;
-        selectElement.appendChild(option);
+        let conversionFormats;
+        switch (file.type.split('/')[0]) {
+            case 'image':
+                conversionFormats = ['JPEG', 'PNG', 'GIF', 'BMP'];
+                break;
+            /*case 'application':
+                conversionFormats = ['DOCX', 'TXT', 'RTF'];
+                break;
+            case 'video':
+                conversionFormats = ['MP4', 'GIF', 'AVI', 'MOV'];
+                break;*/
+            case 'audio':
+                conversionFormats = ['WAV', 'MP3'];
+                break;
+            default:
+                conversionFormats = [];
+                break;
+        }
+
+        const selectElement = document.getElementById('formatSelect');
+        selectElement.innerHTML = '';
+
+        conversionFormats.forEach(format => {
+            const option = document.createElement('option');
+            option.value = format;
+            option.textContent = format;
+            selectElement.appendChild(option);
     });
+    }
 }
 
 function handleFileDrop(event) {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
 
-    document.getElementById('fileName').innerText = `${file.name}`;
-    document.getElementById('fileSize').innerText = `${(file.size * 0.0000010).toFixed(2)}MB`;
-    document.getElementById('fileFormat').innerText = `${file.type.split('/')[0]}`;
+    var supportedFormats = ['JPEG', 'PNG', 'GIF', 'BMP', 'WAV', 'MP3'];
 
-    document.getElementById('zone').style.display = 'none';
-    document.getElementById('converterPanel').style.display = 'block';
-
-    let conversionFormats;
-    switch (file.type.split('/')[0]) {
-        case 'image':
-            conversionFormats = ['JPEG', 'PNG', 'GIF', 'BMP'];
-            break;
-        case 'application':
-            conversionFormats = ['PDF', 'DOCX', 'TXT', 'RTF'];
-            break;
-        case 'video':
-            conversionFormats = ['MP4', 'GIF', 'AVI', 'MOV'];
-            break;
-        case 'audio':
-            conversionFormats = ['WAV', 'MP3'];
-            break;
-        default:
-            conversionFormats = ['None'];
-            break;
+    if (!supportedFormats.includes(file.type.split('/')[1].toUpperCase())) {
+        document.getElementById('unsup').style.display = 'block';
     }
+    else {
+        document.getElementById('fileName').innerText = `${file.name}`;
+        document.getElementById('fileSize').innerText = `${(file.size * 0.0000010).toFixed(2)}MB`;
+        document.getElementById('fileFormat').innerText = `${file.type.split('/')[0]}`;
 
-    const selectElement = document.getElementById('formatSelect');
-    selectElement.innerHTML = '';
+        document.getElementById('unsup').style.display = 'none';
+        document.getElementById('zone').style.display = 'none';
+        document.getElementById('converterPanel').style.display = 'block';
 
-    conversionFormats.forEach(format => {
-        const option = document.createElement('option');
-        option.value = format;
-        option.textContent = format;
-        selectElement.appendChild(option);
+        let conversionFormats;
+        switch (file.type.split('/')[0]) {
+            case 'image':
+                conversionFormats = ['JPEG', 'PNG', 'GIF', 'BMP'];
+                break;
+            /*case 'application':
+                conversionFormats = ['DOCX', 'TXT', 'RTF'];
+                break;
+            case 'video':
+                conversionFormats = ['MP4', 'GIF', 'AVI', 'MOV'];
+                break;*/
+            case 'audio':
+                conversionFormats = ['WAV', 'MP3'];
+                break;
+            default:
+                conversionFormats = [];
+                break;
+        }
+
+        const selectElement = document.getElementById('formatSelect');
+        selectElement.innerHTML = '';
+
+        conversionFormats.forEach(format => {
+            const option = document.createElement('option');
+            option.value = format;
+            option.textContent = format;
+            selectElement.appendChild(option);
     });
+    }
 }
 
 document.getElementById('convertButton').addEventListener('click', function() {
